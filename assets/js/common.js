529144d6b0d84a72f390f8bfe0dbf1340c6df22a
Alexa Andig <contact@alexaandig.ml>
2023-07-15 00:31:36 +0800

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,15 +2,14 @@
 
 function loadCommonElements() {
     // Load header
-        var headerElement = document.getElementById("header");
-            fetch("header.html")
-                    .then(response => response.text())
-                            .then(html => headerElement.innerHTML = html);
+    var headerElement = document.getElementById("header");
+    fetch("header.html")
+        .then(response => response.text())
+        .then(html => headerElement.innerHTML = html);
 
-                                // Load footer
-                                    var footerElement = document.getElementById("footer");
-                                        fetch("footer.html")
-                                                .then(response => response.text())
-                                                        .then(html => footerElement.innerHTML = html);
-                                                        }
-                                                        
+    // Load footer
+    var footerElement = document.getElementById("footer");
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(html => footerElement.innerHTML = html);
+}
