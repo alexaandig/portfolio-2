6d590704be07aac4bc6edd1ac8e0f2acf0cf7098
Alexa Andig <contact@alexaandig.ml>
2023-07-15 01:15:20 +0800

    Update component.js

--- assets/js/component.js
+++ assets/js/component.js
@@ -1,15 +1,51 @@
-// common.js
+// Define the custom element class
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+    
+    // Create a shadow root
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    
+    // Clone the template content and attach it to the shadow root
+    const template = document.createElement('template');
+    template.innerHTML = `
+      <style>
+        /* Define component styles here */
+        nav {
+          background-color: #f2f2f2;
+          padding: 10px;
+        }
 
-function loadCommonElements() {
-    // Load header
-    var headerElement = document.getElementById("header");
-    fetch("header.html")
-        .then(response => response.text())
-        .then(html => headerElement.innerHTML = html);
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+        }
 
-    // Load footer
-    var footerElement = document.getElementById("footer");
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(html => footerElement.innerHTML = html);
+        li {
+          display: inline;
+          margin-right: 10px;
+        }
+
+        a {
+          text-decoration: none;
+          color: #333;
+          padding: 5px;
+        }
+      </style>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    `;
+
+    shadowRoot.appendChild(template.content.cloneNode(true));
+  }
 }
+
+// Register the custom element
+customElements.define('nav-bar', NavBar);
